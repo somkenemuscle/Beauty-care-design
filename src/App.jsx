919f922acc4212cc518affe286ee6f7b692ff601
Somkene Ojukwu <somkeneoj@gmail.com>
2024-04-30 02:31:27 +0100

    font chnages,new section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/navbar/navbar';
 import Carousel from './components/carousel/carousel';
 import Footer from './components/footer/footer';
-
+import ProductCards from './components/productCards/productCards';
 function App() {
   return (
     <div>
@@ -18,11 +18,51 @@ function App() {
       {/* carousel section */}
       <Carousel />
 
+      {/* best sellers section */}
+      <section id='best-sellers-section'>
+        <h4>Meet Our Best Sellers</h4>
+        <div className="container text-center">
+          <div className="row">
+            <div className="best-sellers-div col-xl-3 col-md-6">
+              <ProductCards
+                src='https://images.unsplash.com/photo-1600428877878-1a0fd85beda8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                name='Vitamin C Serum'
+                price='8.15'
+              />
+            </div>
+            <div className="best-sellers-div col-xl-3 col-md-6">
+              <ProductCards
+                src='https://images.unsplash.com/photo-1564594206491-26734df9237f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                name='Tumeric Face Mask'
+                price='12.45'
+              />
+            </div>
+            <div className="best-sellers-div col-xl-3 col-md-6">
+              <ProductCards
+                src='https://images.unsplash.com/photo-1629732097571-b042b35aa3ed?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                name='The Collagen Booster'
+                price='87.00'
+              />
+            </div>
+            <div className="best-sellers-div col-xl-3 col-md-6">
+              <ProductCards
+                src='https://images.unsplash.com/photo-1629732047847-50219e9c5aef?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjExfHxza2luY2FyZSUyMHByb2R1Y3RzfGVufDB8fDB8fHww'
+                name='Ultra Moisturizer '
+                price='30.00'
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+    
       {/* promo section */}
       <section id='promo-section'>
         <h2>OUR SERVICES</h2>
         <p>Get 20% discount on all products which will be effective from 22nd April till 5th May 2024</p>
       </section>
+
+
 
 
       {/* footer section */}
---
+++ src/components/productCards/productCards.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { HiOutlineShoppingCart } from "react-icons/hi";
+import { FaStar } from "react-icons/fa";
+function productCards({ src, name, price }) {
+  return (
+    <div className="card">
+      <img src={src} className="card-img-top" alt="Fissure in Sandstone" />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">PRICE: $ {price} USD </p>
+        <p style={{ color: 'whitesmoke' }}><FaStar /><FaStar /><FaStar /><FaStar /><span style={{ color: '#897663' }}><FaStar /></span></p>
+        <a href="/" className="btn btn-sm btn-dark" data-mdb-ripple-init>ADD TO CART  <HiOutlineShoppingCart /></a>
+      </div>
+    </div>
+  )
+}
+
+export default productCards
